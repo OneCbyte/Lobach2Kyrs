3ddc041396f13381ba32d136fbcf749068d99888
unknown <T2387@D113.local>
2026-09-03 14:03:46 +0300

    Update Main

--- Lobach2Kyrs.cpp
+++ Lobach2Kyrs.cpp
@@ -2,5 +2,12 @@
 
 int main()
 {
+    int a, b;
     std::cout << "Hello World!\n";
+    std::cout << "Input a: ";
+    std::cin >> a;
+    std::cout << "Input b: ";
+    std::cin >> b;
+    std::cout << "Result " << a + b;
+    return 0;
 }
